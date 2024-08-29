f889c5f3d15ac9287756d7a3b64fb4bd0c597133
monir178 <monir.mzs17@gmail.com>
2024-08-29 17:59:30 +0600

    meta data added

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -5,31 +5,20 @@ import { NextRequest } from 'next/server'
 export const getServerSideUser = async (
     cookies: NextRequest['cookies'] | ReadonlyRequestCookies
 ) => {
-    const token = cookies.get('payload-token')?.value;
+    const token = cookies.get('payload-token')?.value
 
-    if (!token) {
-        return { user: null }; // Handle the case where there's no token
-    }
-
-    try {
-        const meRes = await fetch(
-            `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
-            {
-                headers: {
-                    Authorization: `JWT ${token}`,
-                },
-            }
-        );
-
-        if (!meRes.ok) {
-            console.error(`Failed to fetch user: ${meRes.statusText}`);
-            return { user: null };
+    const meRes = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+        {
+            headers: {
+                Authorization: `JWT ${token}`,
+            },
         }
+    )
 
-        const { user } = (await meRes.json()) as { user: User | null };
-        return { user };
-    } catch (error) {
-        console.error('An error occurred while fetching the user:', error);
-        return { user: null }; // Return null user if there's an error
+    const { user } = (await meRes.json()) as {
+        user: User | null
     }
-};
+
+    return { user }
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -20,5 +21,49 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2
   }).format(numericPrice);
+}
+
+
+export function constructMetadata({
+  title = 'DigiStore - the marketplace for digital assets',
+  description = 'Discover DigiStore, where every digital product is handpicked and vetted to meet our   uncompromising quality standards. Elevate your projects with assets that inspire and perform.',
+  image = '/thumbnail.jpg',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@joshtriedcoding',
+    },
+    icons,
+    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
 
